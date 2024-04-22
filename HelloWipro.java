52ea6d4566fcb2c2d8795f6b2dd186cb9401f6b7
AparnaMa24 <166710040+AparnaMa24@users.noreply.github.com>
2024-04-22 13:31:53 +0000

    Update HelloWipro.java

--- HelloWipro.java
+++ HelloWipro.java
@@ -1,9 +1,9 @@
 
-public class Jenkins {
+public class HelloWipro {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("Hello World");
+System.out.println("Hello Wipro");
 	}
 
 }
